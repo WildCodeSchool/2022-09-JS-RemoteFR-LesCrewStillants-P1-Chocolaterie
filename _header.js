1ff6cdbe4f2457b5ef5306e05847c48386711988
Laynaria <pro.laynaria@outlook.fr>
2022-09-30 10:45:50 +0200

    nav bar nearly done, only need to fix logo part

--- _header.js
+++ _header.js
@@ -1,6 +1,6 @@
 // Selecting Header and injecting a logo into it
 const header = document.querySelector("header");
-header.innerHTML = "<span>&#9776</span>";
+header.innerHTML = `<span id="menuBurger">&#9776</span>`;
 header.appendChild(document.createElement("img")).src = "./Assets/Logo.png";
 
 // Selecting a logo with a class in case its needed later and inserting a nav into header
@@ -22,14 +22,32 @@ const navItems = [
 
 // We make a loop to insert link based on array length
 for (let i = 0; i < navItems.length; i++) {
+  //add a li in ul for each loop with a class name based on the loop
   ul.appendChild(document.createElement("li")).classList.add("li" + (i + 1));
+  //select the li based on the loop
   const currentLi = document.querySelector(".li" + (i + 1));
+  //add a "a" in current Li of the loop
   currentLi
     .appendChild(document.createElement("a"))
     .classList.add(navItems[i].class);
+  //select current a based on the loop
   const currentA = document.querySelector(".menu" + (i + 1));
+  // inject current a with an href and innerText from the array navItems
   currentA.href = `${navItems[i].link}`;
   currentA.innerText = `${navItems[i].name}`;
-
-  console.log(currentLi);
 }
+
+//Select menuBurger icon and add make a let variable to know if the menu is shown or hidden
+const menuBurger = document.querySelector("#menuBurger");
+let menuIndicator = 2;
+
+menuBurger.addEventListener("click", () => {
+  console.log("click");
+  if (menuIndicator < 2) {
+    nav.style.left = "-600px";
+    menuIndicator++;
+  } else {
+    nav.style.left = "0px";
+    menuIndicator--;
+  }
+});
